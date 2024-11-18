4cf0617974f27a21cdd0caf8b7047b40ed96e3dc
DoheumPark <pirbak@hanmail.net>
2024-11-18 09:39:05 +0900

    book - 11-15

--- src/main/java/com/green/madang/manager/book/model/BookGetReq.java
+++ src/main/java/com/green/madang/manager/book/model/BookGetReq.java
@@ -1,6 +1,7 @@
 package com.green.madang.manager.book.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,10 +9,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BookGetReq {
-    private int page; //실제 받는 값
-    private int size; //실제 받는 값
+    @Parameter(name = "페이지값", description = "선택된 페이지값", example = "1", required = true)
+    private int page;
+    @Parameter(name = "페이지 당 아이템 수", description = "한 페이지마다 보여질 아이템 수", example = "30", required = true)
+    private int size;
+    @Parameter(name = "검색 텍스트", description = "도서 제목에서 검색을 한다.", example = "검색 텍스트")
     private String searchText;
     @JsonIgnore //Swagger 문서에서 멤버필드가 안 나왔으면 할 때 이 애노테이션을 붙이면 된다.
     private int startIdx; //(page - 1) * size
-
 }
--- src/main/java/com/green/madang/manager/book/model/BookPostReq.java
+++ src/main/java/com/green/madang/manager/book/model/BookPostReq.java
@@ -7,10 +7,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BookPostReq {
-    @Schema(title = "도서 이름", description = "설명할거 있으면 적으며 된다.")
+    @Schema(title = "도서 이름", description = "설명할거 있으면 적으며 된다.", example = "학교 종이 땡땡땡!", requiredMode = Schema.RequiredMode.REQUIRED)
     private String bookName;
-    @Schema(title = "출판사 이름")
+    @Schema(title = "출판사 이름", example = "한빛출판사", requiredMode = Schema.RequiredMode.REQUIRED)
     private String publisher;
-    @Schema(title = "도서 가격")
+    @Schema(title = "도서 가격", example = "12000", requiredMode = Schema.RequiredMode.REQUIRED)
     private int price;
 }
